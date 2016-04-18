4394c59a071cf84fa2bdd01f6727d1885680d7da
AntonYaskevich <wtfurdoing@gmail.com>
2016-04-18 20:03:11 +0200

    rewrite

--- src/main/java/ru/yandex/zhmyd/App.java
+++ src/main/java/ru/yandex/zhmyd/App.java
@@ -11,22 +11,24 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import ru.yandex.zhmyd.entity.Entity;
+import org.hibernate.Session;
+import ru.yandex.zhmyd.entity.User;
 
 public class App extends Application
 {
     private TableView table = new TableView();
 
-    private final ObservableList<Entity> data =
+    private final ObservableList<User> data =
             FXCollections.observableArrayList(
-                    new Entity("Jacob", "Smith", "jacob.smith@example.com"),
-                    new Entity("Isabella", "Johnson", "isabella.johnson@example.com"),
-                    new Entity("Ethan", "Williams", "ethan.williams@example.com"),
-                    new Entity("Emma", "Jones", "emma.jones@example.com"),
-                    new Entity("Michael", "Brown", "michael.brown@example.com")
+                    new User("Jacob", "Smith", "jacob.smith@example.com"),
+                    new User("Isabella", "Johnson", "isabella.johnson@example.com"),
+                    new User("Ethan", "Williams", "ethan.williams@example.com"),
+                    new User("Emma", "Jones", "emma.jones@example.com"),
+                    new User("Michael", "Brown", "michael.brown@example.com")
             );
 
     public static void main( String[] args )
@@ -35,66 +37,83 @@ public class App extends Application
     }
 
     public void start(Stage stage) throws Exception {
+
+        HibernateUtil.getSessionFactory();
+
         Scene scene = new Scene(new Group());
         stage.setTitle("Table View Sample");
-        stage.setWidth(300);
+        stage.setWidth(500);
         stage.setHeight(500);
 
         final Label label = new Label("Address Book");
         label.setFont(new Font("Arial", 20));
+/*
 
+
+
+
+ */
         table.setEditable(true);
 
         TableColumn firstNameCol = new TableColumn("First Name");
         TableColumn lastNameCol = new TableColumn("Last Name");
         TableColumn emailCol = new TableColumn("Email");
+
+
         final TextField addFirstName = new TextField();
         addFirstName.setPromptText("First Name");
         addFirstName.setMaxWidth(firstNameCol.getPrefWidth());
+
         final TextField addLastName = new TextField();
         addLastName.setMaxWidth(lastNameCol.getPrefWidth());
         addLastName.setPromptText("Last Name");
+
         final TextField addEmail = new TextField();
         addEmail.setMaxWidth(emailCol.getPrefWidth());
         addEmail.setPromptText("Email");
 
         firstNameCol.setCellValueFactory(
-                new PropertyValueFactory<Entity,String>("firstName")
+                new PropertyValueFactory<User,String>("firstName")
         );
+
         firstNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
+
         firstNameCol.setOnEditCommit(
-                new EventHandler<TableColumn.CellEditEvent<Entity, String>>() {
-                    public void handle(TableColumn.CellEditEvent<Entity, String> t) {
-                        ((Entity) t.getTableView().getItems().get(
+                new EventHandler<TableColumn.CellEditEvent<User, String>>() {
+                    public void handle(TableColumn.CellEditEvent<User, String> t) {
+                        ((User) t.getTableView().getItems().get(
                                 t.getTablePosition().getRow())
                         ).setFirstName(t.getNewValue());
                     }
                 }
         );
+
+
         lastNameCol.setCellValueFactory(
-                new PropertyValueFactory<Entity,String>("lastName")
+                new PropertyValueFactory<User,String>("lastName")
         );
         lastNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
-        lastNameCol.setOnEditCommit( new EventHandler<TableColumn.CellEditEvent<Entity, String>>() {
-            public void handle(TableColumn.CellEditEvent<Entity, String> t) {
-                ((Entity) t.getTableView().getItems().get(
+        lastNameCol.setOnEditCommit( new EventHandler<TableColumn.CellEditEvent<User, String>>() {
+            public void handle(TableColumn.CellEditEvent<User, String> t) {
+                ((User) t.getTableView().getItems().get(
                         t.getTablePosition().getRow())
                 ).setLastName(t.getNewValue());
             }
         });
         emailCol.setCellValueFactory(
-                new PropertyValueFactory<Entity,String>("email")
+                new PropertyValueFactory<User,String>("email")
         );
 
         table.getColumns().addAll(firstNameCol, lastNameCol, emailCol);
 
         table.setItems(data);
 
+
         final Button button = new Button("Add");
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
-                data.add(new Entity(
+                data.add(new User(
                         addFirstName.getText(),
                         addLastName.getText(),
                         addEmail.getText()));
@@ -104,10 +123,12 @@ public class App extends Application
             }
         });
 
+        HBox hBox = new HBox();
+        hBox.getChildren().addAll(addFirstName, addLastName, addEmail, button);
         final VBox vbox = new VBox();
         vbox.setSpacing(5);
         vbox.setPadding(new Insets(10, 0, 0, 10));
-        vbox.getChildren().addAll(table,addFirstName, addLastName, addEmail, button);
+        vbox.getChildren().addAll(table,hBox);
 
         ((Group) scene.getRoot()).getChildren().addAll(vbox);
 
---
+++ src/main/java/ru/yandex/zhmyd/HibernateUtil.java
@@ -0,0 +1,27 @@
+package ru.yandex.zhmyd;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import ru.yandex.zhmyd.entity.User;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+                sessionFactory = new AnnotationConfiguration().addPackage("ru.yandex.zhmyd.entity").addAnnotatedClass(User.class)
+                        .configure().buildSessionFactory();
+            } catch (Throwable ex) {
+                System.err.println("Initial SessionFactory creation failed." + ex);
+                throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void close(){
+        sessionFactory.close();
+    }
+}
